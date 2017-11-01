e194321888b6c83071eae75fbcc885b44c603913
haazime <h4zime@gmail.com>
2017-11-01 20:15:37 +0900

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   resources :sign_up_requests, only: [:new, :create]
 
-  get '/sign_up/:token', to: 'sign_ups#create', as: :sign_ups
+  get '/sign_up/:token', to: 'sign_ups#create', as: :sign_up
   get '/sign_in/:token', to: 'sessions#create', as: :sign_in
 
   resource :home, only: [:show]
