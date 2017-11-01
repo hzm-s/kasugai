a12b1a0488d525ecd1dfc3f2cc6efe29803d7c9a
haazime <h4zime@gmail.com>
2017-11-01 18:15:52 +0900

    fix message

--- spec/features/sign_up_with_email_spec.rb
+++ spec/features/sign_up_with_email_spec.rb
@@ -1,22 +1,42 @@
 require 'rails_helper'
 
 describe 'メールアドレスで登録する' do
-  it do
-    name = 'ユーザーA'
-    email = 'user.a@gmail.com'
+  let(:name) { 'ユーザーA' }
+  let(:email) { 'user.a@gmail.com' }
 
-    visit new_sign_up_path
-    fill_in 'sign_up[name]', with: name
-    fill_in 'sign_up[email]', with: email
-    perform_enqueued_jobs do
-      click_on '登録する'
+  context '未登録の場合' do
+    it do
+      visit new_sign_up_path
+      fill_in 'sign_up[name]', with: name
+      fill_in 'sign_up[email]', with: email
+      perform_enqueued_jobs do
+        click_on '登録する'
+      end
+
+      expect(page).to have_content('ユーザー登録確認メールを送信しました。')
+
+      open_email(email)
+      current_email.click_link 'こちらから登録を完了してください'
+
+      expect(page).to have_content(name)
     end
+  end
 
-    expect(page).to have_content('メールを送信しました')
+  xcontext '登録済みの場合' do
+    it do
+      visit new_sign_up_path
+      fill_in 'sign_up[name]', with: name
+      fill_in 'sign_up[email]', with: email
+      perform_enqueued_jobs do
+        click_on '登録する'
+      end
 
-    open_email(email)
-    current_email.click_link 'こちらから登録を完了してください'
+      expect(page).to have_content('メールを送信しました')
 
-    expect(page).to have_content(name)
+      open_email(email)
+      current_email.click_link 'こちらから登録を完了してください'
+
+      expect(page).to have_content(name)
+    end
   end
 end
