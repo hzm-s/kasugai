636e6cdeb87362c6846f6517fe8e1ba5e70a8224
haazime <h4zime@gmail.com>
2017-11-02 18:34:33 +0900

    add tests for expiration

--- spec/models/sign_up_finder_spec.rb
+++ spec/models/sign_up_finder_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'Find SignUp' do
   describe '.find_available' do
-    it do
+    it 'メールアドレス1つにつき1つのレコード' do
       sign_up_a1 = SignUp.create!(name: 'name a', email: 'a@example.com')
       sign_up_a2 = SignUp.create!(name: 'name a', email: 'a@example.com')
       sign_up_b = SignUp.create!(name: 'name b', email: 'b@example.com')
@@ -11,6 +11,28 @@ describe 'Find SignUp' do
         expect(SignUp.find_available(sign_up_a1.token)).to be_nil
         expect(SignUp.find_available(sign_up_a2.token)).to_not be_nil
         expect(SignUp.find_available(sign_up_b.token)).to_not be_nil
+      end
+    end
+
+    it '15分00秒経過は有効' do
+      now = Time.current
+      sign_up = nil
+      Timecop.travel(now) do
+        sign_up = SignUp.create!(name: 'name', email: 'email')
+      end
+      Timecop.travel(now + 15.minutes) do
+        expect(SignUp.find_available(sign_up.token)).to_not be_nil
+      end
+    end
+
+    it '15分01秒経過は無効' do
+      now = Time.current
+      sign_up = nil
+      Timecop.travel(now) do
+        sign_up = SignUp.create!(name: 'name', email: 'email')
+      end
+      Timecop.travel(now + 15.minutes + 1.second) do
+        expect(SignUp.find_available(sign_up.token)).to be_nil
       end
     end
   end
