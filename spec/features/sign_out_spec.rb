cedec2491ae1237496f8010fdd9ec553f580fc61
haazime <h4zime@gmail.com>
2017-11-06 14:43:20 +0900

    impl

---
+++ spec/features/sign_out_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe 'ログアウト' do
+  it do
+    user = sign_up
+    sign_in(user)
+    find('#app-sign-out').click
+
+    aggregate_failures do
+      expect(page).to have_content('ログアウトしました')
+      expect(page).to_not have_content(user.name)
+    end
+  end
+end
