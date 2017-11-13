1833b42f7350a765d7372894bdce2af15139217c
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2017-11-13 14:14:17 +0900

    add breadcrumbs

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -7,6 +7,16 @@ crumb :project_issues do |project|
   parent :project_home, project
 end
 
+crumb :project_members do |project|
+  link t('project_navs.members'), project_members_url(project)
+  parent :project_home, project
+end
+
+crumb :show_issue do |project, issue|
+  link t('project_navs.show_issue'), project_issue_url(project, issue)
+  parent :project_issues, project
+end
+
 crumb :new_issue do |project|
   link t('project_navs.new_issue'), new_project_issue_url(project)
   parent :project_issues, project
@@ -15,9 +25,4 @@ end
 crumb :edit_issue do |project, issue|
   link t('project_navs.edit_issue'), edit_project_issue_url(project, issue)
   parent :project_issues, project
-end
-
-crumb :project_members do |project|
-  link t('project_navs.members'), project_members_url(project)
-  parent :project_home, project
 end
