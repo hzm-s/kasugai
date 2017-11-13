2212cd888992ac02e8aab8437c104b9c455620c2
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2017-11-13 14:00:06 +0900

    fix ui

--- app/assets/javascripts/setupLoading.js
+++ app/assets/javascripts/setupLoading.js
@@ -1,8 +1,10 @@
 var setupLoading = function() {
   $('[data-behavior="loader"]').each(function(i, el) {
     var width = $(el).outerWidth();
+    var height = $(el).outerHeight();
     $(el).on('click', function(e) {
       $(e.target).css('width', width);
+      $(e.target).css('height', height);
     });
   });
 }
