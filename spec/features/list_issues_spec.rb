97c344f8a4f53a6aba9deea0adea5a257d666f19
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2017-11-17 13:31:55 +0900

    show comment count

--- spec/features/list_issues_spec.rb
+++ spec/features/list_issues_spec.rb
@@ -8,11 +8,14 @@ describe '課題一覧' do
       issue_a = create_issue(user_a, project_a, title: '課題A')
       issue_b = create_issue(user_b, project_b, title: '課題B')
 
+      post_comment(user_a, issue_a, content: 'Comment')
+
       sign_in(user_a)
       visit project_issues_path(project_id: project_a)
 
       aggregate_failures do
         expect(first('.app_issue_title').text).to eq(issue_a.title)
+        expect(first('.app_issue_comment_count').text).to eq('1')
         expect(page).to_not have_content(issue_b.title)
       end
     end
