eb0056da050b8efb7cc1fe551316787cd4e67bb6
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2017-11-21 16:10:48 +0900

    fix word

--- spec/features/close_issue_spec.rb
+++ spec/features/close_issue_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe '課題のアーカイブ' do
+describe '課題の解決' do
   it do
     user = sign_up
     project = create_project(user, name: 'P')
@@ -9,10 +9,10 @@ describe '課題のアーカイブ' do
     sign_in(user)
     visit project_issue_path(project, issue)
 
-    click_on 'この課題を解決にする'
+    click_on 'この課題を解決済みにする'
 
     aggregate_failures do
-      expect(page).to have_content('課題を解決にしました')
+      expect(page).to have_content('課題を解決済みにしました')
       expect(page).to_not have_content(issue.title)
     end
   end
