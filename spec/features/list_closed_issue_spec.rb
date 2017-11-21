81d126b62daba49f1bb14bbf6b60b33b408506c8
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2017-11-21 14:30:45 +0900

    prepare

---
+++ spec/features/list_closed_issue_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe '解決した課題の一覧' do
+  context do
+    include_context '2人のユーザーがそれぞれプロジェクトを作成している'
+
+    it do
+      #closed = create_issue(user_a.member_of(project_a))
+    end
+  end
+end
