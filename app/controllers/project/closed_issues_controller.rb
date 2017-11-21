937a9b6a16eb7a99e31380aa0b04b0b5a11f8076
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2017-11-21 15:10:29 +0900

    closed issue list

--- app/controllers/project/closed_issues_controller.rb
+++ app/controllers/project/closed_issues_controller.rb
@@ -2,6 +2,10 @@ class Project::ClosedIssuesController < Project::BaseController
   before_action :ensure_signed_in, only: [:create]
   before_action :ensure_project_member, only: [:create]
 
+  def index
+    @issues = ClosedIssue.for_project(current_project.id)
+  end
+
   def create
     IssueService.close(current_issue)
     redirect_to project_issues_url(current_project), notice: flash_message
--- app/models/closed_issue.rb
+++ app/models/closed_issue.rb
@@ -1,2 +1,14 @@
 class ClosedIssue < ApplicationRecord
+  belongs_to :issue
+
+  delegate :title, to: :issue
+
+  class << self
+
+    def for_project(project_id)
+      joins(:issue)
+        .where(issues: { project_id: project_id })
+        .order(created_at: :desc)
+    end
+  end
 end
