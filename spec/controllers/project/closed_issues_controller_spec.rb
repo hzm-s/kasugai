cc6344af9e88a4a296816627979916e5de207e57
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2017-11-22 15:13:55 +0900

    guard

--- spec/controllers/project/closed_issues_controller_spec.rb
+++ spec/controllers/project/closed_issues_controller_spec.rb
@@ -7,6 +7,11 @@ describe Project::ClosedIssuesController do
   end
 
   it do
+    get :show, params: { project_id: 'dummy', id: 'dummy' }
+    expect_ensure_signed_in
+  end
+
+  it do
     post :create, params: { project_id: 'dummy' }
     expect_ensure_signed_in
   end
@@ -23,6 +28,11 @@ describe Project::ClosedIssuesController do
 
     it do
       get :index, params: { project_id: project_a }
+      expect_ensure_project_member
+    end
+
+    it do
+      get :show, params: { project_id: project_a, id: 'dummy' }
       expect_ensure_project_member
     end
 
