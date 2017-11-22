7846be3dad07ad40cb7d926538ae0fcdc50d4b35
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2017-11-22 13:48:56 +0900

    refactor

--- spec/services/issue_service_spec.rb
+++ spec/services/issue_service_spec.rb
@@ -1,20 +1,20 @@
 require 'rails_helper'
 
 describe IssueService do
-  it do
-    user = sign_up
-    project = create_project(user, name: 'P')
-    member = user.as_member_of(project)
+  let(:user) { sign_up }
+  let(:member) { user.as_member_of(project) }
+  let(:project) { create_project(user, name: 'P') }
 
-    issue_a = create_issue(member, title: 'A')
-    issue_b = create_issue(member, title: 'B')
-    issue_c = create_issue(member, title: 'C')
+  describe '#change_priority' do
+    it do
+      issue_a = create_issue(member, title: 'A')
+      issue_b = create_issue(member, title: 'B')
+      issue_c = create_issue(member, title: 'C')
+      close_issue(issue_a)
 
-    close_issue(issue_a)
-
-    described_class.change_priority(issue_b, 1)
-
-    ordered_issues = Issue.for_project(project.id).map(&:title)
-    expect(ordered_issues).to eq(%w(C B))
+      described_class.change_priority(issue_b, 1)
+      ordered_issues = Issue.for_project(project.id).map(&:title)
+      expect(ordered_issues).to eq(%w(C B))
+    end
   end
 end
