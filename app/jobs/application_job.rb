4b0599639ae861e463972a6563139e1e78b25006
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2017-11-29 13:48:17 +0900

    refactor

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,2 +1,8 @@
 class ApplicationJob < ActiveJob::Base
+
+  protected
+
+    def render(*args, &block)
+      ApplicationController.renderer.render(*args, &block)
+    end
 end
--- app/jobs/issue_comment_broadcast_job.rb
+++ app/jobs/issue_comment_broadcast_job.rb
@@ -2,10 +2,7 @@ class IssueCommentBroadcastJob < ApplicationJob
   queue_as :default
 
   def perform(comment)
-    new_comment_html =
-      ApplicationController
-        .renderer
-        .render(partial: 'issue/comments/others_comment', locals: { comment: comment })
+    new_comment_html = render(partial: 'issue/comments/others_comment', locals: { comment: comment })
     IssueCommentsChannel.broadcast_to(
       comment.issue,
       html: new_comment_html,
