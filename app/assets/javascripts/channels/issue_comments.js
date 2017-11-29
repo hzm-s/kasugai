2d2fede5b391466e1a5a9bf9d66228c5ab663f48
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2017-11-29 12:49:40 +0900

    coffee => js

---
+++ app/assets/javascripts/channels/issue_comments.js
@@ -0,0 +1,45 @@
+App.issueComments = App.cable.subscriptions.create("IssueCommentsChannel", {
+  container: function() {
+    return $("[data-channel='issue-comments']")
+  },
+
+  list: function() {
+    return $('#js-issue-comments')
+  },
+
+  connected: function() {
+    setTimeout(function() {
+      App.issueComments.start();
+      App.issueComments.installPageChangeCallback();
+    }, 1000);
+  },
+
+  disconnected: function() {},
+
+  received: function(data) {
+    if (!this.userIsCurrentUser(data.html)) {
+      Helpers.appendNode(this.list(), $(data.html));
+    }
+  },
+
+  userIsCurrentUser: function(html) {
+    var authorId = $(html).attr('data-author-id');
+    var currentUserId = $('meta[name=current-user]').attr('id');
+    return authorId == currentUserId;
+  },
+
+  start: function() {
+    var issueId = this.container().data('issue-id');
+    if (issueId) {
+      this.perform('follow', { issue_id: issueId });
+    } else {
+      this.perform('unfollow');
+    };
+  },
+
+  installPageChangeCallback: function() {
+    $(document).on('turbolinks:load', function() {
+      App.issueComments.start();
+    });
+  },
+});
