acc4269114baeb56d2a9f4c19a40f7496d775595
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2017-12-05 13:51:50 +0900

    wip

--- app/models/activity_list/project.rb
+++ app/models/activity_list/project.rb
@@ -2,19 +2,11 @@ class ActivityList::Project < ApplicationRecord
   belongs_to :daily_list, class_name: 'ActivityList::Daily', foreign_key: :activity_list_daily_id
   belongs_to :project, class_name: '::Project', foreign_key: :project_id
 
-  has_many :activities, -> { order(id: :desc) }, class_name: 'ProjectActivity', foreign_key: :activity_list_project_id, dependent: :destroy
+  has_many :activities, -> { order(id: :desc) },
+    class_name: 'ProjectActivity', foreign_key: :activity_list_project_id,
+    dependent: :destroy
 
   delegate :name, to: :project, prefix: true
-
-  class << self
-
-    def for_user(user_id)
-      project_ids = Project.project_ids_for_user(user_id)
-      joins(:daily_list)
-        .where(project_id: project_ids)
-        .order(id: :desc)
-    end
-  end
 end
 
 __END__
